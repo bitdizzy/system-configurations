065c352e922072b2fe7717ee55d7b3eefe81ed83
bitdizzy <dev@bitdizzy.io>
2021-05-30 15:22:47 -0400

    Disable all caches but nixos.org

--- system/common/nix.nix
+++ system/common/nix.nix
@@ -3,13 +3,13 @@
 {
   nix = {
     binaryCachePublicKeys = [
-      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
-      "1365.webhop.org-1:jrz/Z6UBFafWsuArR96ghoQdNCkPlxKB0IwXdM4zAsc="
+      # "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+      # "1365.webhop.org-1:jrz/Z6UBFafWsuArR96ghoQdNCkPlxKB0IwXdM4zAsc="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
     binaryCaches = [
       "https://cache.nixos.org/"
-      "https://nixcache.reflex-frp.org"
+      # "https://nixcache.reflex-frp.org"
       # "http://1365.webhop.org:5000"
     ];
     nixPath = [
