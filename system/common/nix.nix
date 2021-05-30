b7f47b80f6e410ac0932d6c359762a650783457e
bitdizzy <dev@bitdizzy.io>
2021-05-30 14:53:30 -0400

    Temporarily disable a nix cache that's down

--- system/common/nix.nix
+++ system/common/nix.nix
@@ -5,18 +5,19 @@
     binaryCachePublicKeys = [
       "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
       "1365.webhop.org-1:jrz/Z6UBFafWsuArR96ghoQdNCkPlxKB0IwXdM4zAsc="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
     binaryCaches = [
       "https://cache.nixos.org/"
       "https://nixcache.reflex-frp.org"
-      "http://1365.webhop.org:5000"
+      # "http://1365.webhop.org:5000"
     ];
     nixPath = [
       "nixpkgs=${nixpkgs}"
       "nixos-config=${configurationSrc}"
       "nixpkgs-overlays=${../../overlays}"
     ];
-    nrBuildUsers = numBuildCores * 2;
+    nrBuildUsers = numBuildCores;
     buildCores = numBuildCores;
     maxJobs = lib.mkDefault (numBuildCores * 2);
     trustedUsers = [ "@wheel" ];
