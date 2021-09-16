63aa95ce94456fecb0de3bb32b0c79d8af9c1981
bitdizzy <dev@bitdizzy.io>
2021-09-15 23:40:29 -0400

    Enable rt kernel

--- system/musnix/default.nix
+++ system/musnix/default.nix
@@ -1,13 +1,13 @@
 { pkgs, lib, ... }:
 
 {
-#  boot.kernelPackages = lib.mkForce (pkgs.linuxPackages_5_4_rt);
-#  musnix.kernel.packages = pkgs.linuxPackages_5_4_rt;
-#  musnix.das_watchdog.enable = true;
-#  musnix.kernel = {
-#    optimize = true;
-#    realtime = true;
-#  };
+  boot.kernelPackages = lib.mkForce (pkgs.linuxPackages_5_9_rt);
+  musnix.kernel.packages = pkgs.linuxPackages_5_9_rt;
+  musnix.das_watchdog.enable = true;
+  musnix.kernel = {
+    optimize = true;
+    realtime = true;
+  };
   musnix.enable = true;
   users.users.min.extraGroups = [ "audio" ];
 
