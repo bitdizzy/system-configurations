b04b4218df9e775fba77d31d821759a2cbe49852
bitdizzy <dev@bitdizzy.io>
2021-12-29 15:47:51 -0500

    Add nginx group to user

--- system/common/users.nix
+++ system/common/users.nix
@@ -5,12 +5,15 @@ in {
 
   users = {
     defaultUserShell = pkgs.zsh;
+    groups = {
+      min = {};
+    };
     users = {
       root.initialHashedPassword = "$6$.Q664jtsxN$MfurQET/NOIUW2gNBAKTdLOqsNNA4fR2BidzOgYrKwMQnCccfM3dQ51ETObKjA2xFIfJBOAeh4Wyzt8X5/GcS0";
       min = {
         name = "min";
         group = "min";
-        extraGroups = [ "wheel" "networkmanager" "fuse" ];
+        extraGroups = [ "wheel" "networkmanager" "fuse" "nginx" ];
         createHome = true;
         home = "/home/min";
         useDefaultShell = true;
