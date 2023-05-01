4344511b74908246eaebd2ce5ca3d06e025705f2
bitdizzy <dev@bitdizzy.io>
2023-05-01 11:29:51 -0400

    switch to intel drivers for isbell

--- system/isbell.nix
+++ system/isbell.nix
@@ -4,7 +4,7 @@ let
   hostName = "isbell";
   numBuildCores = 6;
   displayDpi = 220;
-  videoDriver = "nvidia";
+  videoDriver = "intel";
   nixpkgs = import ../deps/nixpkgs-unstable/thunk.nix;
 in {
   ### Sucks
