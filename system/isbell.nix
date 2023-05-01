48783f309e249c3f0c45f99b9906d4ca4d7e05eb
bitdizzy <dev@bitdizzy.io>
2023-05-01 11:39:55 -0400

    Bump

--- system/isbell.nix
+++ system/isbell.nix
@@ -4,7 +4,19 @@ let
   hostName = "isbell";
   numBuildCores = 6;
   displayDpi = 220;
-  videoDriver = "intel";
+  videoDriver = "nvidia";
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    modesetting.enabel = true;
+    prime = {
+      offload.enable = true;
+
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+
   nixpkgs = import ../deps/nixpkgs-unstable/thunk.nix;
 in {
   ### Sucks
